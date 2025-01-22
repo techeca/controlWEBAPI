fb9875d450e79089b76b5431f93158c4ffe1ca43
techeca <jp.vasquez@outlook.com>
2025-01-22 00:17:06 -0300

    add deleteUser endpoint and mid for errors

--- controllers/User.js
+++ controllers/User.js
@@ -8,7 +8,7 @@ export async function CreateUser(req, res, next) {
         if (!correo || !rut || !nombre || !tipo || !contrasena || !apellido) return next(new Error("Error al obtener datos de usuario"))
         const userExist = await FindUserByRUT(rut, false);
         //Si encuentra un usuario envía 409 y error: 'Usuario ya registrado'   
-        if (userExist) { return res.status(409).json({ error: "Usuario ya registrado" }) }
+        if (userExist) { return next({ status: 409, message: "Usuario ya registrado" }) }
 
         const hashedPassword = await bcrypt.hash(contrasena, 10);
         const newUserData = {
@@ -18,8 +18,8 @@ export async function CreateUser(req, res, next) {
             password: hashedPassword, // Guardar la contraseña hasheada
             type: tipo,
             lastName: apellido,
-            lastName: segundoApellido,
-            surName: segundoNombre,
+            surName: segundoApellido,
+            secondName: segundoNombre,
             cargo: cargo
         };
 
@@ -27,16 +27,15 @@ export async function CreateUser(req, res, next) {
         const newUser = await prisma.user.create({ data: newUserData })
         res.status(200).json({ result: newUser })
     } catch (error) {
-        console.error(error);
-        next("Error al crear el usuario")
+        next({ status: 500, message: "Error interno al crear el usuario", details: error.message });
     } finally {
         prisma.$disconnect();
     }
 }
 
 export async function ReadUser(req, res, next) {
+    const { rut } = req.body;
     try {
-        const { rut } = req.body;
         if (!rut) return next(new Error("Error al obtener datos de usuario"))
         const userData = await FindUserByRUT(userRUT, false);
         //Si No encuentra usuario envía 404 y error: "Usuario no encontrado"
@@ -44,9 +43,33 @@ export async function ReadUser(req, res, next) {
         //Si encuentra un usuario envía 200 con sus datos
         res.status(200).json({ result: userData })
     } catch (error) {
-        //En caso de error va a middleware "Error al crear el usuario"
         console.error(error);
-        next(new Error("Error al buscar el usuario"))
+        next({ status: 500, message: "Error al buscar el usuario", details: error.message })
+    }
+}
+
+export async function DeleteUser(req, res, next){
+    const prisma = new PrismaClient();
+    const { rut } = req.params;
+    try {
+        await prisma.user.delete({
+            where: { rut: rut}
+        })
+        res.status(200).json({ message: "Usuario eliminado"})
+    } catch (error) {
+        next({ status: 500, message: "Error al eliminar el usuario", details: error.message})
+    }
+}
+
+export async function GetAllUsers(req, res, next){
+    const prisma = new PrismaClient()
+    try {
+        const allUsers = await prisma.user.findMany()
+        res.status(200).json({ result: allUsers})
+    } catch (error) {
+        next({ status: 500, message: "Error al obtener los usuarios", details: error.message})
+    } finally {
+        prisma.$disconnect();
     }
 }
 
@@ -62,7 +85,6 @@ async function FindUserByRUT(rut, withControls) {
         })
         return user
     } catch (error) {
-        console.error(error);
         return null
     } finally {
         prisma.$disconnect()
