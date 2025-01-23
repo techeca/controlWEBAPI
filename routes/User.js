307fb39e38d2d2f05bb28e132b94a30bae9446b3
techeca <jp.vasquez@outlook.com>
2025-01-22 23:47:56 -0300

    fix endpoints and add update

--- routes/User.js
+++ routes/User.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { CreateUser, DeleteUser, GetAllUsers, ReadUser } from '../controllers/User.js';
+import { CreateUser, DeleteUser, GetAllUsers, ReadUser, UpdateUser } from '../controllers/User.js';
 import { authorizeRole } from '../middlewares/Authorization.js';
 import { ROLES } from '../utils/contants.js';
 import { authenticateToken } from '../middlewares/jwt.js';
@@ -11,7 +11,8 @@ userRouter.use(authorizeRole(ROLES.ADMIN))
 
 //userRouter.get('/read', ReadUser);
 userRouter.get('/all', GetAllUsers);
-userRouter.post('/create', CreateUser);
+userRouter.post('/', CreateUser);
 userRouter.delete('/:rut', DeleteUser);
+userRouter.put('/:rut', UpdateUser)
 
 export default userRouter;
